d48b09d5ce28c57f5d3b31f825321004065c39df
Sean Smith <seanwssmith@gmail.com>
2017-04-29 15:30:47 -0400

    The Moral Majority is neither.

--- app.py
+++ app.py
@@ -5,5 +5,19 @@ app = Flask(__name__)
 def hello():
 	return render_template('index.html')
 
+@app.route("/c")
+def c():
+	code = """
+main() {
+	printf("Hello World!")
+}"""
+	return render_template('index.html', code=code)
+
+@app.route("/python")
+def python():
+	code = """print "Hello World!"
+	"""
+	return render_template('index.html', code=code)
+
 if __name__ == "__main__":
     app.run(debug=True)
